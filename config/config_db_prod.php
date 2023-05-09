64846e573adbe1e796776351a13c7b5b7126259a
q-code <phvandenberghe@gmail.com>
2023-05-09 18:27:29 +0200

    Delete config_db_prod.php

--- config/config_db_prod.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('QDB_SYSTEM','pdo.mysql');
-define('QDB_HOST','clustermysql07.hosteur.com');
-define('QDB_DATABASE','qtcut');
-define('QDB_PREFIX','');
-define('QDB_USER','qtcut');
-define('QDB_PWD','hfahr4510h');
-define('QDB_INSTALL ','2014-12-27');
